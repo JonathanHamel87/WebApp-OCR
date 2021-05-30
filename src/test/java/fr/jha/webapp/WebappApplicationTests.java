8506a289eb55ca8b58100e2b43866a05c871d709
Jonathan Hamel <jonathan.hamel87@gmail.com>
2021-05-30 15:11:29 +0200

    initialisation

---
+++ src/test/java/fr/jha/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jha.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
